3bcc5572e8d903409abf35ee44cf601224aba91c
Evan A Martin <evanamartin@gmail.com>
2021-07-26 15:43:05 -0700

    Add functions to perform hierarchical clustering

--- R/cluster.R
+++ R/cluster.R
@@ -1,19 +1,31 @@
-# x: A list with three elements output from the x_47 function. The first element
-# is the the data matrix with the aligned retention times and recalibrated mass.
-# The second and third elements are the median of the ppm and retention time
-# errors respectively. These values are calculated by Dataset.
-#
-# method: A character string indicating what agglomeration method should be used
-# in the hclust function.
-#
-# height: An number specifying the height at which the tree created by the
-# hclust function should be cut.
-#
-# min_size: An integer value indicating the minimum number of points a cluster
-# must have. All clusters with fewer members than min_size will be reclassified
-# as "noise" points.
-x_50 <- function (x, method, height, min_size) {
-  
+#' ...
+#'
+#' ...
+#'
+#' @param x A list with three elements output from the x_47 function. The first
+#'   element is the the data matrix with the aligned retention times and
+#'   recalibrated mass. The second and third elements are the median of the ppm
+#'   and retention time errors respectively. These values are calculated by
+#'   Dataset.
+#'
+#' @param method A character string indicating what agglomeration method should
+#'   be used in the hclust function.
+#'
+#' @param height An number specifying the height at which the tree created by
+#'   the hclust function should be cut.
+#'
+#' @param min_size An integer value indicating the minimum number of points a
+#'   cluster must have. All clusters with fewer members than min_size will be
+#'   reclassified as "noise" points.
+#'
+#' @return ...
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+#'
+cluster <- function (x, method, height, min_size) {
+
   x_cluster <- x[[1]] %>%
     group_by(Gene) %>%
     # Normalize the mass according to the ppm error that was computed in the
@@ -44,10 +56,10 @@ x_50 <- function (x, method, height, min_size) {
     mutate(cluster = list(replace(cluster, cluster %in% noise, 0))) %>%
     select(Gene, data, cluster) %>%
     unnest(cols = c(data, cluster))
-  
+
   # Return the cluster data frame along with the ppm and rt errors.
   return (list(x = x_cluster,
                ppm_error = x[[2]],
                rt_error = x[[3]]))
-  
+
 }
