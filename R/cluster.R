9b2a1d98465f2d0429163fdd9f2898e68c7d1c79
Evan A Martin <evanamartin@gmail.com>
2021-07-26 16:38:51 -0700

    Explicitly call dplyr and stats functions

--- R/cluster.R
+++ R/cluster.R
@@ -27,35 +27,37 @@
 cluster <- function (x, method, height, min_size) {
 
   x_cluster <- x[[1]] %>%
-    group_by(Gene) %>%
+    dplyr::group_by(Gene) %>%
     # Normalize the mass according to the ppm error that was computed in the
     # x_47 function. The normalized recalibrated mass will be used for
     # clustering. This means the h argument in the cutree function will
     # correspond to the standard deviation.
-    mutate(NormRecalMass = log10(RecalMass) / log10(1 + x[[2]] / 1e6)) %>%
+    dplyr::mutate(NormRecalMass = log10(RecalMass) / log10(1 + x[[2]] / 1e6)) %>%
     # Normalize the aligned rt according to the rt error that was computed in
     # the x_47 function. The normalized rt will be used for clustering. This
     # means the h argument in the cutree function will correspond to the
     # standard deviation.
-    mutate(NormRTalign = RTalign / x[[3]]) %>%
-    ungroup() %>%
-    nest_by(Gene) %>%
-    mutate(hcluster = list(hclust(dist(select(data,
-                                              NormRTalign,
-                                              NormRecalMass)),
-                                  method = method))) %>%
-    mutate(cluster = list(cutree(hcluster, h = height))) %>%
+    dplyr::mutate(NormRTalign = RTalign / x[[3]]) %>%
+    dplyr::ungroup() %>%
+    dplyr::nest_by(Gene) %>%
+    dplyr::mutate(
+      hcluster = list(stats::hclust(stats::dist(dplyr::select(data,
+                                                              NormRTalign,
+                                                              NormRecalMass)),
+                                    method = method))
+    ) %>%
+    dplyr::mutate(cluster = list(stats::cutree(hcluster, h = height))) %>%
     # Find all clusters that have fewer than min_size members. In the next step
     # these points will be reclassified as "noise" points.
-    mutate(noise = list(tibble(clust = cluster) %>%
-                          group_by(clust) %>%
-                          tally() %>%
-                          filter(n < min_size) %>%
-                          pull(clust))) %>%
+    dplyr::mutate(noise = list(tibble::tibble(clust = cluster) %>%
+                                 dplyr::group_by(clust) %>%
+                                 dplyr::tally() %>%
+                                 dplyr::filter(n < min_size) %>%
+                                 dplyr::pull(clust))) %>%
     # Convert all clusters with fewer than min_size members to cluster 0.
-    mutate(cluster = list(replace(cluster, cluster %in% noise, 0))) %>%
-    select(Gene, data, cluster) %>%
-    unnest(cols = c(data, cluster))
+    dplyr::mutate(cluster = list(replace(cluster, cluster %in% noise, 0))) %>%
+    dplyr::select(Gene, data, cluster) %>%
+    dplyr::unnest(cols = c(data, cluster))
 
   # Return the cluster data frame along with the ppm and rt errors.
   return (list(x = x_cluster,
