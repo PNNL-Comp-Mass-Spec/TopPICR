fd295c28587aa6c07d1d6e725de1a85c15d8e22b
Evan A Martin <evanamartin@gmail.com>
2021-10-14 16:59:53 -0700

    Don't use a reference data set when recalibrating the mass

--- R/recalibrate_mass.R
+++ R/recalibrate_mass.R
@@ -25,56 +25,46 @@
 #'
 calc_error <- function (x, ref_ds) {
 
-  # Compute the robust median of the precursor mass within Dataset and
-  # Proteoform. This variable will be used to calculate the error (in ppm)
-  # between the reference and all other data sets.
-  x_abridged <- x %>%
-    dplyr::select(c(Dataset, Proteoform, `Precursor mass`,
-                    `Adjusted precursor mass`)) %>%
-    dplyr::group_by(Dataset, Proteoform) %>%
-    # Calculate a representative mass (repMass) within each Dataset and
-    # Proteoform. This mass will be used to recalibrate the mass in the
-    # recalibrate_mass function.
-    dplyr::summarize(
-      repMass = robust_median(mass = `Precursor mass`,
-                           adj_mass = `Adjusted precursor mass`)
-    )
-
-  # Combine the data frames with the robust median mass and the aligned
-  # retention times.
-  x_mass <- x %>%
-    dplyr::ungroup() %>%
-    dplyr::inner_join(x_abridged)
-
-  # Extract the reference data set.
-  x_ref <- x_mass %>%
-    dplyr::filter(Dataset == ref_ds) %>%
-    dplyr::select(-Dataset)
-
-  # Compute the mass and retention time errors.
-  x_error <- x_mass %>%
+  # Compute the mass error in ppm by data set. This is the difference between
+  # the `Precursor mass` and the `Adjusted precursor mass` converted to parts
+  # per million. Also computes the median and standard deviation of the ppm
+  # errors (by Dataset).
+  ppm_stats <- x %>%
+    dplyr::filter(`#unexpected modifications` == 0) %>%
+    dplyr::mutate(ppm_error = (1e6 * (`Precursor mass` -
+                                       `Adjusted precursor mass`) /
+                                 `Adjusted precursor mass`)) %>%
     dplyr::group_by(Dataset) %>%
-    dplyr::summarize(ppm_rt_error(mass = repMass,
-                                  rt = RTalign,
-                                  pf = Proteoform,
-                                  x_ref = x_ref)) %>%
+    dplyr::summarize(ppm_sd = stats::mad(ppm_error),
+                     ppm_median = stats::median(ppm_error)) %>%
     dplyr::ungroup()
 
-  # Compute the median of the mass and retention time errors across all data
-  # sets (excluding the reference data set). These values will be used in the
-  # cluster functions to determine a mass/retention time envelope around each
-  # cluster.
-  errors <- x_error %>%
-    dplyr::filter(Dataset != ref_ds) %>%
-    dplyr::summarize(ppm_error = stats::median(ppm_mad),
-                     rt_error = stats::median(rt_mad))
+  # Extract the reference data set.
+  x_ref <- x %>%
+    dplyr::filter(Dataset == ref_ds) %>%
+    dplyr::select(RTalign, Proteoform, `Feature intensity`, `Feature apex`)
 
-  # Return the x_error data frame that contains the RecalMass variable along
+  # Compute the median absolute deviation of the retention time errors by data
+  # set then take the median of the mad values.
+  rt_stats <- x %>%
+    dplyr::group_by(Dataset) %>%
+    dplyr::summarize(rt_sd = calc_rt_stats(rt = RTalign,
+                                           pf = Proteoform,
+                                           fi = `Feature intensity`,
+                                           fa = `Feature apex`,
+                                           x_ref = x_ref)) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(Dataset != ref_ds) %>%
+    dplyr::summarize(the_sd = stats::median(rt_sd)) %>%
+    dplyr::pull(the_sd)
+
+  # Return the x_error data frame that contains the repMass variable along
   # with the ppm and retention time errors.
-  return (list(rep_mass = dplyr::select(x_mass, Dataset, repMass),
-               ds_error = x_error,
-               ppm_error = errors$ppm_error[[1]],
-               rt_error = errors$rt_error[[1]]))
+  return (list(
+    ppm_error = ppm_stats,
+    ppm_sd = stats::median(ppm_stats$ppm_sd),
+    rt_sd = rt_stats
+  ))
 
 }
 
@@ -89,16 +79,16 @@ calc_error <- function (x, ref_ds) {
 #'
 #' @param errors A list output from the \code{calc_error} function.
 #'
-#' @param var_name A character string. The name of the mass variable that will
-#'   be recalibrated to the reference data set (e.g., repMass).
+#' @param repMass Logical. If TRUE a representative mass will be calculated from
+#'   the \code{RecalMass} variable. Rows will be grouped by \code{Proteoform}.
 #'
 #' @return A \code{data.table} containing the recalibrated mass variable. The
 #'   following variables were added/removed:
 #'
 #'   | Added             | Removed                    |
 #'   | ----------------- | -------------------------- |
-#'   | `repMass` (when var_name = "repMass") |        |
 #'   | `RecalMass`       |                            |
+#'   | `repMass` (when repMass = TRUE) |              |
 #'
 #' @md
 #'
@@ -106,45 +96,61 @@ calc_error <- function (x, ref_ds) {
 #'
 #' @export
 #'
-recalibrate_mass <- function (x, errors, var_name = "repMass") {
+recalibrate_mass <- function (x, errors, repMass = TRUE) {
 
-  # Check to make sure the data sets in x are also in errors$ds_error. If the
+  # Check to make sure the data sets in x are also in errors$ppm_stats. If the
   # data sets in each data frame are not identical the mass cannot be
   # recalibrated. Here x is the data.table output by the align_rt function and
-  # errors$ds_error is the data.table output by the calc_error function.
-  if (!setequal(unique(x$Dataset), unique(errors$ds_error$Dataset))) {
+  # errors$ppm_stats is the data.table output by the calc_error function.
+  if (!setequal(unique(x$Dataset), unique(errors$ppm_error$Dataset))) {
 
-    stop (paste("The data sets present in x and errors$ds_error do not match."))
-
-  }
-
-  # Check if the variable that will be recalibrated is repMass. If it is the
-  # repMass variable must be added to x.
-  if (var_name == "repMass") {
-
-    # Add the representative mass variable (repMass) to the data matrix. This
-    # variable was created in the calc_error function and is the default mass
-    # used to recalibrate the mass.
-    x$repMass <- errors$rep_mass$repMass
+    stop (paste("The data sets present in x and errors$ppm_error do not match."))
 
   }
 
   # Add the ppm_median variable to the data. This variable will be used to carry
   # out the actual recalibration.
   x_recal <-  dplyr::inner_join(x = x,
-                                y = errors$ds_error,
+                                y = errors$ppm_error,
                                 by = "Dataset") %>%
     # Recalibrate the mass.
     dplyr::mutate(
-      RecalMass = !!rlang::sym(var_name) * (1 - ppm_median / 1e6)
+      RecalMass = `Precursor mass` * (1 - ppm_median / 1e6)
     ) %>%
     # Remove intermediate variables used to recalibrate the mass. These
     # variables are in the output of the calc_error function and can be accessed
     # there if they are needed.
-    dplyr::select(-ppm_mad, -ppm_median, -rt_mad)
+    dplyr::select(-ppm_sd, -ppm_median)
 
-  # Return the x_recal data frame that contains the RecalMass variable.
-  return (x_recal)
+  # Check if the representative mass will be calculated.
+  if (repMass) {
+
+    # Compute the robust median of the precursor mass within Dataset and
+    # Proteoform. This variable will be used to calculate the error (in ppm)
+    # between the reference and all other data sets.
+    x_abridged <- x_recal %>%
+      dplyr::select(c(Dataset, Proteoform, RecalMass,
+                      `Adjusted precursor mass`)) %>%
+      dplyr::group_by(Dataset, Proteoform) %>%
+      # Calculate a representative mass (repMass) within each Dataset and
+      # Proteoform. This mass will be used to recalibrate the mass in the
+      # recalibrate_mass function.
+      dplyr::summarize(
+        repMass = robust_median(mass = RecalMass,
+                                adj_mass = `Adjusted precursor mass`)
+      )
+
+    # Combine the data frames with the robust median mass and the aligned
+    # retention times.
+    x_mass <- x_recal %>%
+      dplyr::ungroup() %>%
+      dplyr::inner_join(x_abridged)
+
+  }
+
+  # Return the x_recal data frame that contains the RecalMass and repMass (when
+  # applicable) variables.
+  return (x_mass)
 
 }
 
@@ -197,38 +203,49 @@ robust_median <- function (mass, adj_mass) {
 
 }
 
-# mass: A vector of masses extracted from the data matrix by the group_by
-# function.
-# rt: A vector of retention times extracted from the data matrix by the group_by
-# function.
-# pf: A vector of proteoforms extracted from the data matrix by the group_by
-# function.
+# rt: A vector of retention times grouped by Dataset.
+# pf: A vector of proteoforms grouped by Dataset.
+# fi: A vector of feature intensity values. These values are grouped by Dataset.
+# fa: A vector of feature apex values. These values are grouped by Dataset.
 # x_ref: the x data matrix filtered by the reference data set.
-ppm_rt_error <- function (mass, rt, pf, x_ref) {
+calc_rt_stats <- function (rt, pf, fi, fa, x_ref) {
 
   # Combine the reference data set along with the data subsetted by the group_by
   # function by Dataset.
-  error <- dplyr::inner_join(x = x_ref[, c("repMass", "RTalign", "Proteoform")],
-                             y = tibble::tibble(repMass = mass,
-                                                RTalign = rt,
-                                                Proteoform = pf),
-                             by = "Proteoform") %>%
-    # Calculate the ppm and retention time errors.
-    dplyr::mutate(ppm_error = 1e6 * (repMass.y - repMass.x) / repMass.x,
-                  rt_error = RTalign.y - RTalign.x)
+  rt_error <- dplyr::inner_join(
+    x = x_ref %>%
+      dplyr::group_by(Proteoform) %>%
+      dplyr::slice_max(`Feature intensity`) %>%
+      # Remove rows where the max `Feature intensity` appears in more than one
+      # row by selecting the min `Feature apex` value.
+      dplyr::slice_min(`Feature apex`) %>%
+      dplyr::select(RTalign, Proteoform) %>%
+      # We only want to keep one row with the repMass value for each Proteoform.
+      # When joining with the non-reference data set the number of rows is
+      # inflated because the join function creates a row for each combination of
+      # values that differ between the two data sets.
+      dplyr::distinct(),
+    y = tibble::tibble(RTalign = rt,
+                       Proteoform = pf,
+                       `Feature intensity` = fi,
+                       `Feature apex` = fa) %>%
+      dplyr::group_by(Proteoform) %>%
+      dplyr::slice_max(`Feature intensity`) %>%
+      # Remove rows where the max `Feature intensity` appears in more than one
+      # row by selecting the min `Feature apex` value.
+      dplyr::slice_min(`Feature apex`) %>%
+      dplyr::select(RTalign, Proteoform) %>%
+      # We only want to keep one row with the repMass value for each Proteoform.
+      # When joining with the non-reference data set the number of rows is
+      # inflated because the join function creates a row for each combination of
+      # values that differ between the two data sets.
+      dplyr::distinct(),
+    by = "Proteoform"
+  ) %>%
+    # Calculate the median absolute deviation of the rt errors.
+    dplyr::mutate(error = RTalign.y - RTalign.x) %>%
+    dplyr::pull(error)
 
-  # Only keep ppm values between plus/minus 20 for computing the median. The
-  # ppm median is used for recalibrating the mass later. We only keep the values
-  # within 20 ppm for a more accurate representation of the group.
-  reduced <- error %>%
-    dplyr::filter(ppm_error > -20 & ppm_error < 20)
-
-  # Calculate the median absolute deviation, median and standard deviation of
-  # the remaining ppm values.
-  mms <- data.frame(ppm_mad = stats::mad(error$ppm_error),
-                    ppm_median = stats::median(reduced$ppm_error),
-                    rt_mad = stats::mad(error$rt_error))
-
-  return (mms)
+  return (stats::mad(rt_error))
 
 }
