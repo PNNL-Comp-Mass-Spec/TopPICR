5914a191282241dc2cf74d30ceb12d3a25bc7d9a
Evan A Martin <evanamartin@gmail.com>
2021-10-29 16:14:29 -0700

    Include a function that combines entire clusters based on a mass/rt envelope and ppm threshold

---
+++ R/generate_groups.R
@@ -0,0 +1,178 @@
+# The main function will create a new column pcg for Proteoform Cluster Group
+# (similar to cluster_new from the merge_clusters function). This function will
+# start with the most abundant cluster's centroid and compare all remaining
+# cluster centroids to it and group them if the centroids fall within the
+# mass/rt envelope of the main cluster. If clusters are grouped with the main
+# cluster their centroids will be removed from the list and will not be
+# considered in creating their own group with other clusters.
+#' ...
+#'
+#' ...
+#'
+#' @param x ...
+#'
+#' @param errors ...
+#'
+#' @param ppm_cutoff ...
+#'
+#' @param n_Da ...
+#'
+#' @param n_rt_sd ...
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+#'
+gen_pcg <- function (x, errors, ppm_cutoff, n_Da, n_rt_sd) {
+
+  x_group <- x %>%
+    dplyr::filter(cluster != 0) %>%
+    dplyr::group_by(Gene) %>%
+    dplyr::mutate(
+      pcGroup = groupate(x = .,
+                         gene = Gene[[1]],
+                         ppm_cutoff = ppm_cutoff,
+                         n_Da = n_Da,
+                         rt_sd = errors$rt_sd,
+                         n_rt_sd = n_rt_sd)
+    )
+
+}
+
+# This function will carry out the bulk of the calculations (sorting, grouping,
+# creating group numbers, ...)
+groupate <- function (x, gene, ppm_cutoff, n_Da, rt_sd, n_rt_sd) {
+
+  c12c13 <- 1.0033548378
+
+  # Naming key/explanation:
+  #
+  # ridley - The data frame containing each cluster centroid. This data frame
+  # will be looped through until all clusters have been grouped. It is possible
+  # that a cluster will not be grouped with any others.
+  #
+  # samus - A data frame that will contain the proteoform/cluster groups. The
+  # proteoform/cluster group variable in this data frame will be returned at the
+  # end of the function after being joined with the input data frame to ensure
+  # it is the proper length.
+  #
+  # metroid - A temporary single row data frame. This data frame contains the
+  # centroid of the current cluster. All remaining centroids in the ridley data
+  # frame will be compared to the centroid in metroid. If any centroid in ridley
+  # falls within the mas/rt envelope and the ppm threshold it will be grouped
+  # with the centroid in metroid.
+  #
+  # metroid_group - Another temporary data frame that contains the rows of
+  # ridley that fall within the mass/rt envelope and below the ppm threshold.
+  # These rows represent clusters that can be combined with the current cluster
+  # (in metroid).
+
+  # Extract all rows corresponding to a given gene.
+  ridley <- x %>%
+    dplyr::filter(Gene == gene) %>%
+    dplyr::group_by(cluster) %>%
+    dplyr::summarize(n_members = dplyr::n(),
+                     cntr_mass = median(RecalMass, na.rm = TRUE),
+                     cntr_rt = median(RTalign, na.rm = TRUE)) %>%
+    dplyr::arrange(-n_members) %>%
+    dplyr::ungroup()
+
+  # Check if there is only one cluster for the given gene. If there is only one
+  # cluster the rest of the algorithm does not need to be run.
+  if (dim(ridley)[[1]] == 1) {
+
+    # Return the original cluster vector because nothing needs to be done to it.
+    return (ridley$cluster)
+
+  }
+
+  # Create an empty tibble that will be added two as the rows of ridley are
+  # looped through.
+  samus <- tibble::tibble(NULL)
+
+  # Loop through each cluster from largest to smallest and group clusters based
+  # on the mass/retention time criteria and ppm cutoff.
+  while (TRUE) {
+
+    # Nab the first row of ridley. The mass/rt centroid of this temporary data
+    # frame will be used to compare with other cluster centroids.
+    metroid <- ridley[1, ]
+
+    # Remove the first row from ridley. This data frame will be used to compare
+    # centroids to the current centroid (found in metroid).
+    ridley <- ridley[-1, ]
+
+    # Check if there are zero rows in ridley. If there are then we have made it
+    # through each of the clusters.
+    if (dim(ridley)[[1]] == 0) {
+
+      # Set the proteoform cluster group to the cluster number of the last
+      # cluster.
+      samus <- metroid %>%
+        dplyr::mutate(pcg = cluster) %>%
+        dplyr::bind_rows(samus, .)
+
+      # Exit the while loop because all clusters have been combined into groups.
+      break
+
+    }
+
+    # Only keep the centroids that fall within the mass/rt envelope and below
+    # the ppm threshold.
+    metroid_group <- ridley %>%
+      dplyr::mutate(diff_mass = abs(metroid$cntr_mass - cntr_mass),
+                    diff_rt = abs(metroid$cntr_rt - cntr_rt)) %>%
+      dplyr::filter(diff_mass < n_Da * c12c13,
+                    diff_rt < n_rt_sd * rt_sd) %>%
+      dplyr::mutate(
+        diff_ppm = abs(
+          (metroid$cntr_mass - cntr_mass -
+             (c12c13 * round(metroid$cntr_mass - cntr_mass,
+                             0))) / metroid$cntr_mass * 1e6
+        )
+      ) %>%
+    dplyr::filter(diff_ppm < ppm_cutoff)
+
+    # Check if there are any clusters that meet the criteria for being grouped
+    # with the current cluster. If there aren't then the current cluster cannot
+    # be grouped with the others and the pcg will be set to the cluster number.
+    if (dim(metroid_group)[[1]] == 0) {
+
+      # Add the current cluster to the data frame that will be the final output.
+      samus <- metroid %>%
+        dplyr::mutate(pcg = cluster) %>%
+        dplyr::bind_rows(samus, .)
+
+      # Runs when there is one or more rows in metroid_group.
+    } else {
+
+      # Combine clusters in a group and add these rows to samus.
+      samus <- metroid_group %>%
+        dplyr::select(-diff_mass, -diff_rt, -diff_ppm) %>%
+        # Add the current cluster data frame (metroid) to the data frame
+        # (metroid_group) of the other clusters that will form a group with the
+        # current cluster.
+        dplyr::bind_rows(metroid, .) %>%
+        dplyr::mutate(pcg = metroid$cluster) %>%
+        dplyr::bind_rows(samus, .)
+
+      # Remove the grouped clusters from the ridley data frame so they are not
+      # considered again.
+      ridley <- ridley[-(which(ridley$cluster %in% metroid_group$cluster)), ]
+
+    }
+
+  } # end while loop
+
+  # Join the data input data frame with samus so the output will be the same
+  # length as the input.
+  suppressMessages(
+    da_end <- x %>%
+      dplyr::filter(Gene == gene) %>%
+      dplyr::inner_join(x = .,
+                        y = dplyr::select(samus, cluster, pcg))
+  )
+
+  return (da_end$pcg)
+
+}
