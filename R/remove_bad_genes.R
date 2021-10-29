f919de027b241a36bd2024aa320b26268d5dea04
Evan A Martin <evanamartin@gmail.com>
2021-10-29 16:12:59 -0700

    Add a function that removes rows with feature intensity values found across multiple genes

---
+++ R/remove_bad_genes.R
@@ -0,0 +1,29 @@
+#' ...
+#'
+#' ...
+#'
+#' @param x ...
+#'
+#' @return ...
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+#'
+rm_false_gene <- function (x) {
+
+  y <- x %>%
+    dplyr::select(Dataset, CV, `Feature apex`, `Feature intensity`,
+                  Gene, `E-value`) %>%
+    dplyr::distinct() %>%
+    dplyr::group_by(Dataset, CV, `Feature apex`, `Feature intensity`) %>%
+    dplyr::slice(which.min(`E-value`)) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(Dataset, CV, `Feature apex`, `Feature intensity`, Gene)
+
+  x2 <- x %>%
+    dplyr::semi_join(y)
+
+  return (x2)
+
+}
