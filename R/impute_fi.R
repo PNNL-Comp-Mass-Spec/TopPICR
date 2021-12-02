7604c8541f7d46b9ff4eeec27d374a662ee850c2
Evan A Martin <evanamartin@gmail.com>
2021-12-02 10:51:52 -0800

    Include Time_apex when matching identified and unidentified features

--- R/impute_fi.R
+++ R/impute_fi.R
@@ -62,7 +62,7 @@ impute_fi <- function(x, feature, ppm_cutoff, rt_cutoff) {
       dplyr::filter(
         abs(RTalign - centroids$meanRT[[e]]) < rt_cutoff
       ) %>%
-      dplyr::select(Dataset, Intensity, CV,
+      dplyr::select(Dataset, Intensity, CV, Time_apex,
                     RTalign, RecalMass) %>%
       dplyr::mutate(
         Gene = centroids$Gene[[e]],
