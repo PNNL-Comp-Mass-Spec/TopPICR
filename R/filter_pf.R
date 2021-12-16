c3f5ebc9ff073f4196f880215b6ac6d5576b09d7
Evan A Martin <evanamartin@gmail.com>
2021-12-15 16:44:05 -0800

    Include a filter by count (gene, cleanSeq, ...) function

---
+++ R/filter_pf.R
@@ -0,0 +1,37 @@
+#' Filter low count proteoforms
+#'
+#' Counts the number of samples each proteoform occurs in and filters out any
+#' proteoforms that occur in fewer samples than the threshold.
+#'
+#' @param x A \code{data.table} output from \code{augment_annotation}.
+#'
+#' @param count_within A character vector specifying the variables that will be
+#'   grouped before counting the number of occurrences of the \code{count}
+#'   variable.
+#'
+#' @param count A character string indicating the variable that will be counted.
+#'
+#' @param threshold An integer. The value that will be used to remove rows whose
+#'   count falls below the threshold.
+#'
+#' @return
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+#'
+filter_by_count <- function (x, count_within, count, threshold) {
+
+  return (
+    x %>%
+      dplyr::select(dplyr::all_of(count_within), !!rlang::sym(count)) %>%
+      dplyr::distinct() %>%
+      dplyr::group_by(!!rlang::sym(count)) %>%
+      dplyr::tally() %>%
+      dplyr::filter(n >= threshold) %>%
+      dplyr::select(-n) %>%
+      dplyr::semi_join(x, .) %>%
+      dplyr::ungroup()
+  )
+
+}
