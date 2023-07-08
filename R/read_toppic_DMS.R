7d3b20e9de26c938529f88d65ae5e0e1660d18ec
Vlad Petyuk <petyuk@gmail.com>
2023-07-07 19:03:58 -0700

    cleaning UniProtAcc

--- R/read_toppic_DMS.R
+++ R/read_toppic_DMS.R
@@ -62,15 +62,6 @@ read_TopPIC_DMS <- function(data_package_num){
 
 
 
-
-  idss <- ids[1700000:1847098,] %>%
-    dplyr::mutate(
-      UniProtAcc = case_when(grepl("\\|", UniProtAcc) ~
-                               purrr::map_chr(stringr::str_split(UniProtAcc, "\\|"), purrr::pluck, 2),
-                                       TRUE ~ UniProtAcc))
-
-
-
   # Extract just the UniProt accession. This is the second element (when
   # splitting by |) of the `Protein accession` variable output by TopPIC.
   ids <- ids %>%
