82103ef2be2f151de11f48e036b9e2fd2d596d9a
Vlad Petyuk <petyuk@gmail.com>
2023-11-02 15:49:08 -0700

    better label alignment

--- R/plot_fns.R
+++ R/plot_fns.R
@@ -531,7 +531,7 @@ plot_accession_ptm <- function (x,
       limits = if (max(prot$ymax) < 0.5) c(0, 0.5) else NULL
     ) +
     ggplot2::theme(
-      axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5),
+      axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1),
       plot.title = ggplot2::element_text(hjust = 0.5, size = 16)
     ) +
     ggplot2::ggtitle(prot_name)
